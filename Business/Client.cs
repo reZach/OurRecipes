80cfd51bd133939cc28a4861315562bf1d21e593
reZach <reZach@users.noreply.github.com>
2021-04-07 19:26:47 -0500

    Add project files.

---
+++ Business/Client.cs
@@ -0,0 +1,23 @@
+﻿using Octokit;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OurRecipes.Business
+{
+    public class Client
+    {
+        public Client()
+        {
+
+        }
+
+        public async void Get()
+        {
+            GitHubClient githubClient = new GitHubClient(new ProductHeaderValue("OurRecipes"));            
+            githubClient.Credentials = new Credentials("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");
+
+            await githubClient.Repository.Content.GetAllContents()
+        }
+    }
+}
